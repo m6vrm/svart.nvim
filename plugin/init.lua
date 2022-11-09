4f82ab9f41fbff39b212867200d1cb1924f72729
Roman Madyanov <romantaken@gmail.com>
2022-11-09 17:22:12 +0300

    Make SvartLabel hl group link to IncSearch by default

--- plugin/init.lua
+++ plugin/init.lua
@@ -3,7 +3,7 @@ local highlights = {
     SvartMatch = { default = true, link = "Search" },
     SvartMatchCursor = { default = true, link = "Cursor" },
     SvartDimmedCursor = { default = true, link = "TermCursorNC" },
-    SvartLabel = { default = true, link = "CurSearch" },
+    SvartLabel = { default = true, link = "IncSearch" },
     SvartRegularPrompt = { default = true, link = "MoreMsg" },
     SvartErrorPrompt = { default = true, link = "ErrorMsg" },
 }
